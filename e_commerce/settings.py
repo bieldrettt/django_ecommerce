545605daf96c9e3c72a04ab0a73101ea4e088d29
Gabriel Faria Mota <gabriel.farialima@hotmail.com>
2019-05-26 19:36:33 -0300

    create and set  .gitignore, _commerce/settings.py, e_commerce/urls.py, e_commerce/views.py, template/home_page.html

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'e_commerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
